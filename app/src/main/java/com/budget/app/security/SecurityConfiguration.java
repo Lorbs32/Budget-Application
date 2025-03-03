f773ab49776b703dcb20f0203d1f5cb3f43c00f3
Mazin <maziniqbal2005@gmail.com>
2025-03-03 02:22:13 -0500

    updated security to allow access to /h2-console

--- app/src/main/java/com/budget/app/security/SecurityConfiguration.java
+++ app/src/main/java/com/budget/app/security/SecurityConfiguration.java
@@ -7,17 +7,25 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableWebSecurity
 public class SecurityConfiguration {
 	@Autowired private CustomUserDetailsService customerUserDetailsService;
 
 	@Bean
 	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 		http
-				.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/login").permitAll().anyRequest().authenticated()) //url specified in requestMatchers do NOT need authentication
+				.csrf(csrf -> csrf
+						.ignoringRequestMatchers("/h2-console/**")
+				)
+				.headers(headers -> headers
+						.frameOptions(frameOptions -> frameOptions.disable())
+				)
+				.authorizeHttpRequests(authorizeRequests -> authorizeRequests.requestMatchers("/h2-console/**").permitAll().anyRequest().authenticated()) //url in requestMatchers does NOT need auth, other DOES need auth
 				.formLogin(form -> form.loginPage("/login").permitAll().defaultSuccessUrl("/dashboard", true))
 				.logout(logout -> logout.logoutUrl("/logout").permitAll());
 		return http.build();
