b5ca24c732d270fdb811c4479cb8e5b5f8aca908
Lorbs32 <lorbs32@gmail.com>
2025-03-04 22:00:06 -0600

    saving nothing

--- app/src/main/java/com/budget/app/AppApplication.java
+++ app/src/main/java/com/budget/app/AppApplication.java
@@ -8,7 +8,7 @@ public class AppApplication
 {
 	public static void main(String[] args)
 	{
-		// ConfigurableApplicationContext configurableAppContext =
+		//ConfigurableApplicationContext configurableAppContext =
 		SpringApplication.run(AppApplication.class, args);
 	}
 }
