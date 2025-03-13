3b0ddef36ebe50e950d647805ff901d4eae6c006
ZinJava <maziniqbal2005@gmail.com>
2025-03-12 21:23:17 -0400

    update pers branch

--- app/src/main/java/com/budget/app/service/BudgetServiceImplementation.java
+++ app/src/main/java/com/budget/app/service/BudgetServiceImplementation.java
@@ -12,55 +12,27 @@ import java.util.List;
 
 @Service
 @Transactional
-<<<<<<< HEAD
 public class BudgetServiceImplementation implements BudgetService {
+	// Autowires
 	@Autowired
 	private UserRepository userRepository;
-
 	@Autowired
 	private BudgetDateRepository budgetDateRepository;
-
 	@Autowired
 	private BudgetRepository budgetRepository;
-
 	@Autowired
-	private GroupRepository groupRepository;
-
+	private CategoryRepository categoryRepository;
 	@Autowired
 	private LineItemRepository lineItemRepository;
-
 	@Autowired
 	private TransactionRepository transactionRepository;
 
+
+	// Interface Method Implementations
 	@Override
 	public List<User> getUsers() {
 		return userRepository.findAll();
 	}
-=======
-public class BudgetServiceImplementation implements BudgetService
-{
-    // Autowires
-    @Autowired
-    private UserRepository userRepository;
-    @Autowired
-    private BudgetDateRepository budgetDateRepository;
-    @Autowired
-    private BudgetRepository budgetRepository;
-    @Autowired
-    private CategoryRepository categoryRepository;
-    @Autowired
-    private LineItemRepository lineItemRepository;
-    @Autowired
-    private TransactionRepository transactionRepository;
-
-
-    // Interface Method Implementations
-    @Override
-    public List<User> getUsers()
-    {
-        return userRepository.findAll();
-    }
->>>>>>> 545dc41d712e7a87617eece74786a0ace40af205
 
 	@Override
 	public User getUserByEmail(String email) {
@@ -71,7 +43,7 @@ public class BudgetServiceImplementation implements BudgetService
 	public List<BudgetDate> getBudgetDatesBetween(LocalDate currentDate) {
 		BudgetDate budgetDate = budgetDateRepository.findByBudgetDateBetween(currentDate);
 		Integer currentBudgetDateId = budgetDate.getId();
-		List<Integer> ids = new ArrayList<>();
+		List<Integer> ids = new ArrayList<Integer>();
 
 		// TO DO need to write a more flexible algorithm here.
 		// This assumes 5 months to display and that the 3 month "march" is the current and selected month.
@@ -91,64 +63,34 @@ public class BudgetServiceImplementation implements BudgetService
 		return budgetRepository.findByUserIdAndBudgetDateId(userId, dateId);
 	}
 
-<<<<<<< HEAD
 	@Override
-	public List<Group> getGroups(int budgetId) {
-		return groupRepository.findById(budgetId);
+	public List<Category> getCategories(int budgetId) {
+		return categoryRepository.findByBudgetId(budgetId);
 	}
 
 	@Override
-	public List<LineItem> getLineItems(List<Group> groups) {
-		List<Integer> ids = new ArrayList<>();
-		for (Group group : groups) {
-			ids.add(group.getId());
+	public List<LineItem> getLineItems(List<Category> categories) {
+		List<Integer> ids = new ArrayList<Integer>();
+		for (Category category : categories) {
+			ids.add(category.getId());
 		}
 		return lineItemRepository.findByIdIn(ids);
 	}
 
 	@Override
 	public List<Transaction> getTransactions(List<LineItem> lineItems) {
-		List<Integer> ids = new ArrayList<>();
+		List<Integer> ids = new ArrayList<Integer>();
 		for (LineItem lineItem : lineItems) {
 			ids.add(lineItem.getId());
 		}
 		return transactionRepository.findByIdIn(ids);
 	}
-=======
-    @Override
-    public List<Category> getCategories(int budgetId)
-    {
-        return categoryRepository.findByBudgetId(budgetId);
-    }
 
-    @Override
-    public List<LineItem> getLineItems(List<Category> categories)
-    {
-        List<Integer> ids = new ArrayList<Integer>();
-        for(Category category : categories)
-        {
-            ids.add(category.getId());
-        }
-        return lineItemRepository.findByIdIn(ids);
-    }
+	// ------------------TO DO-------------------
 
-    @Override
-    public List<Transaction> getTransactions(List<LineItem> lineItems)
-    {
-        List<Integer> ids = new ArrayList<Integer>();
-        for(LineItem lineItem: lineItems)
-        {
-            ids.add(lineItem.getId());
-        }
-        return transactionRepository.findByIdIn(ids);
-    }
-
-    // ------------------TO DO-------------------
-
-    // calculateBudgetSummary(Int budgetId)
-        // Use repo to fetch all line items for a budget
-        // Separate income from expenses using isIncome true or false
-        // Calculate totals for income and expenses
-        // Calculate remaining balance
->>>>>>> 545dc41d712e7a87617eece74786a0ace40af205
+	// calculateBudgetSummary(Int budgetId)
+	// Use repo to fetch all line items for a budget
+	// Separate income from expenses using isIncome true or false
+	// Calculate totals for income and expenses
+	// Calculate remaining balance
 }
