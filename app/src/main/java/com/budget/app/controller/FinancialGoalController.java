821ac333c262008be08407a8f635aafa3efcd2a2
Mazin <maziniqbal2005@gmail.com>
2025-04-05 12:26:26 -0400

    switch to pc

--- app/src/main/java/com/budget/app/controller/FinancialGoalController.java
+++ app/src/main/java/com/budget/app/controller/FinancialGoalController.java
@@ -1,15 +1,14 @@
 package com.budget.app.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/financialgoals")
 public class FinancialGoalController {
-	@GetMapping("/goals")
-	public String goals(Model model) {
-		return "financialgoals";
+	@PostMapping("/create")
+	public String create() {
+		return "";
 	}
 }
